87f97458eb987a9becaa0df75739b5607d472156
kenzychew <kenzychew@gmail.com>
2025-02-10 00:29:53 +0800

    feat: signin page

---
+++ controllers/authController.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    return res.send("Username already taken.");
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+
+  const user = await User.create(req.body);
+  res.send(`Thanks for signing up ${user.username}`);
+  // res.send("Form submission accepted!");
+});
+
+router.get("/login", (req, res) => {
+  res.render("auth/login.ejs");
+});
+
+router.post("/login", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+
+  // There is a user AND they had the correct password. Time to make a session!
+  // Avoid storing the password, even in hashed format, in the session
+  // If there is other data you want to save to `req.session.user`, do so here!
+  req.session.user = {
+    username: userInDatabase.username,
+  };
+  res.redirect("/");
+});
+
+module.exports = router;
